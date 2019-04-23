f39a7888a03f0ac7dc051335341a87b54c050806
MarcusJones <jones.0bj3@gmail.com>
2019-04-23 16:53:01 +0200

    update installed deps to Pipfile, change spawner config

--- testing/jupyterhub_config.py
+++ testing/jupyterhub_config.py
@@ -13,6 +13,10 @@ c.JupyterHub.authenticator_class = DummyAuthenticator
 # Optionally set a global password that all users must use
 # c.DummyAuthenticator.password = "your_password"
 
-from jupyterhub.spawners import SimpleSpawner
+#from jupyterhub.spawners import SimpleSpawner
 
-c.JupyterHub.spawner_class = SimpleSpawner
+#c.JupyterHub.spawner_class = SimpleSpawner
+
+from simplespawner import SimpleLocalProcessSpawner
+
+c.JupyterHub.spawner_class = SimpleLocalProcessSpawner
